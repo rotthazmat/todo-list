8e6c5df5906372a79a31ca433d9cc0521a36398a
Rott Hazmat <dev.costume@gmail.com>
2026-04-26 13:34:54 -0500

    Fix export to middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-export function proxy(request: NextRequest) {
+export function middleware(request: NextRequest) {
   const token = request.headers.get('authorization')?.replace('Bearer ', '');
 
   if (token !== process.env.API_SECRET) {
